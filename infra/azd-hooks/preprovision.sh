6f4dc6fb308b7006d6e65657a6b81984fdcdeab2
Jon Gallant <2163001+jongio@users.noreply.github.com>
2024-01-19 18:49:30 +0000

    only provision state first time

--- infra/azd-hooks/preprovision.sh
+++ infra/azd-hooks/preprovision.sh
@@ -13,31 +13,32 @@ if [ "$AI_ONLY" = "false" ]; then
     az feature register --namespace Microsoft.ContainerService --name NetworkObservabilityPreview
     az extension add --upgrade --name aks-preview
     az extension add --upgrade --name amg
+else
+    # Define the file path
+    TF_DIR="infra/tfstate"
+
+    # Set TF_VAR_location to the value of AZURE_LOCATION
+    export TF_VAR_location=$AZURE_LOCATION
+
+    # Set TF_VAR_environment_name to the value of AZURE_ENV_NAME
+    export TF_VAR_environment_name=$AZURE_ENV_NAME
+
+    # Initialize and apply Terraform configuration
+    terraform -chdir="$TF_DIR" init
+    terraform -chdir="$TF_DIR" apply -auto-approve
+
+        # Add a delay to ensure that the service is up and running
+    echo "Waiting for the service to be available..."
+    sleep 30
+
+    # Capture the outputs
+    RS_STORAGE_ACCOUNT=$(terraform -chdir="$TF_DIR" output -raw RS_STORAGE_ACCOUNT )
+    RS_CONTAINER_NAME=$(terraform -chdir="$TF_DIR" output -raw RS_CONTAINER_NAME )
+    RS_RESOURCE_GROUP=$(terraform -chdir="$TF_DIR" output -raw RS_RESOURCE_GROUP )
+
+    # Set the environment variables
+    azd env set RS_STORAGE_ACCOUNT "$RS_STORAGE_ACCOUNT"
+    azd env set RS_CONTAINER_NAME "$RS_CONTAINER_NAME"
+    azd env set RS_RESOURCE_GROUP "$RS_RESOURCE_GROUP"
 fi
 
-# Define the file path
-TF_DIR="infra/tfstate"
-
-# Set TF_VAR_location to the value of AZURE_LOCATION
-export TF_VAR_location=$AZURE_LOCATION
-
-# Set TF_VAR_environment_name to the value of AZURE_ENV_NAME
-export TF_VAR_environment_name=$AZURE_ENV_NAME
-
-# Initialize and apply Terraform configuration
-terraform -chdir="$TF_DIR" init
-terraform -chdir="$TF_DIR" apply -auto-approve
-
-    # Add a delay to ensure that the service is up and running
-echo "Waiting for the service to be available..."
-sleep 30
-
-# Capture the outputs
-RS_STORAGE_ACCOUNT=$(terraform -chdir="$TF_DIR" output -raw RS_STORAGE_ACCOUNT )
-RS_CONTAINER_NAME=$(terraform -chdir="$TF_DIR" output -raw RS_CONTAINER_NAME )
-RS_RESOURCE_GROUP=$(terraform -chdir="$TF_DIR" output -raw RS_RESOURCE_GROUP )
-
-# Set the environment variables
-azd env set RS_STORAGE_ACCOUNT "$RS_STORAGE_ACCOUNT"
-azd env set RS_CONTAINER_NAME "$RS_CONTAINER_NAME"
-azd env set RS_RESOURCE_GROUP "$RS_RESOURCE_GROUP"
